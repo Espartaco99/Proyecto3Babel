31d0437f5d79419b7a4f5dca8599e0afe488399f
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-04-10 11:07:56 +0200

    sass arreglado

--- Proyecto Salto Base/app/worker.js
+++ Proyecto Salto Base/app/worker.js
@@ -1,6 +1,9 @@
 
 setInterval(() => {
     var date = new Date();
-    var mensaje = date.getHours() +":"+date.getMinutes() +":" + date.getSeconds() + "   " +date.getDate() +"/" + (date.getMonth()+1)  + "/" + date.getFullYear()
+    var hora = date.getHours() +":"+date.getMinutes() +":";
+    if( date.getSeconds()<10) hora += "0" +  date.getSeconds();
+    else hora +=   date.getSeconds();
+    var mensaje = hora + "   " +date.getDate() +"/" + (date.getMonth()+1)  + "/" + date.getFullYear()
     postMessage(mensaje);
 },1000);
