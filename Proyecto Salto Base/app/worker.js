fea3d219e591f3c2dbbcf3045b4957e6caaae883
Diego Garzon Molina <g.molina.diego@gmail.com>
2018-04-11 11:17:50 +0200

    version final

--- Proyecto Salto Base/app/worker.js
+++ Proyecto Salto Base/app/worker.js
@@ -1,9 +1,11 @@
 
 setInterval(() => {
     var date = new Date();
-    var hora = date.getHours() +":"+date.getMinutes() +":";
-    if( date.getSeconds()<10) hora += "0" +  date.getSeconds();
-    else hora +=   date.getSeconds();
+    var hora = date.getHours() +":";
+    if( date.getMinutes()<10) hora += "0" +  date.getMinutes();
+    else hora +=   date.getMinutes();
+    if( date.getSeconds()<10) hora += ":0" +  date.getSeconds();
+    else hora += ":" +   date.getSeconds();
     var mensaje = hora + "   " +date.getDate() +"/" + (date.getMonth()+1)  + "/" + date.getFullYear()
     postMessage(mensaje);
 },1000);
